c2c6cf72417493e572adb5dc33d2530bbf10d960
Mateo-Pulvirenti <mateopulvirenti37@gmail.com>
2021-11-16 09:49:13 -0300

    desafio de la clase n°1 de javascrpt, CODERHOUSE

---
+++ desafios/desafioClase1/main.js
@@ -0,0 +1,8 @@
+let nombre = prompt('Ingrese su nombre');
+let apellido = prompt('Ingrese su apellido');
+let edad = prompt('Ingrese su edad');
+let infoDelUsuario = alert(`DATOS PERSONALES
+Nombre: ${nombre}
+Apellido: ${apellido}
+Edad: ${edad}`)
+
